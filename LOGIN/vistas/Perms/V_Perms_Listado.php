0634000160d8451d521a4077cb6c6b8634889619
TheToaster <a25371@svalero.com>
2024-03-14 21:47:39 +0100

    Added conditional checkbox

--- LOGIN/vistas/Perms/V_Perms_Listado.php
+++ LOGIN/vistas/Perms/V_Perms_Listado.php
@@ -3,11 +3,11 @@ $perms = $datos['perms']['perms'];
 $PUser = $datos['perms']['PUser'];
 $PRol = $datos['perms']['PRol'];
 
-// if(empty($PRol)){
-//         echo "PRol is empty";
-//     } else {
-//         echo "PRol is not empty";
-//     }
+// if (strlen(trim($PRol)) > 0) {
+//         echo "PRol has content other than whitespace";
+// } else {
+//         echo "PRol is empty or not set";
+// }
 
 
 
@@ -18,10 +18,6 @@ $PRol = $datos['perms']['PRol'];
         // Further improvement: Fields are auto-complete
 
 // Checkmark specs: Default value read from session, modify on the fly (onclick)
-
-
-
-
 
 
 // Ordenamos por 'orden' para que copie el menu.
@@ -35,17 +31,21 @@ foreach ($perms as $id_menu => $permisos) {
         echo "<div id= 'patata'>";
         echo "<p>{$permisos[0]['titulo']} || con ID: {$permisos[0]['id_menu']}</p>";
 
-        echo "<div id= 'BotonesMenu'>";
-        if (!isset($permisos['hijos'])) {
-                echo '<td><button class="PermsBoton" onclick="getInsertSubMenu(' . $permisos[0]['id_menu'] . ')"><img class="PermsImagen" src="img/add.png"></button></td>';
+        if (strlen(trim($PRol)) > 0) {
+        } else if (strlen(trim($PUser)) > 0) {
+        } else {
+                echo "<div id= 'BotonesMenu'>";
+                if (!isset($permisos['hijos'])) {
+                        echo '<td><button class="PermsBoton" onclick="getInsertSubMenu(' . $permisos[0]['id_menu'] . ')"><img class="PermsImagen" src="img/add.png"></button></td>';
+                }
+                echo '<td><button class="PermsBoton" onclick="getUpdateMenu(' . $permisos[0]['id_menu'] . ')"><img class="PermsImagen" src="img/option.png"></button></td>';
+                echo '<td><button class="PermsBoton" onclick="getDeleteMenu(' . $permisos[0]['id_menu'] . ')"><img class="PermsImagen" src="img/eckis.png"></button></td>';
+                echo "</div>";
+                // Botones del padre (ID menu)
+                echo "<div id= 'CajaBtn'>"; //Creamos nuevo permiso para el menu
+                echo '<td><button class="PermsBoton" onclick="getInsertPerms(' . $permisos[0]['id_menu'] . ')"><img class="PermsImagen" src="img/create.png"></button></td>';
+                echo "</div>";
         }
-        echo '<td><button class="PermsBoton" onclick="getUpdateMenu(' . $permisos[0]['id_menu'] . ')"><img class="PermsImagen" src="img/option.png"></button></td>';
-        echo '<td><button class="PermsBoton" onclick="getDeleteMenu(' . $permisos[0]['id_menu'] . ')"><img class="PermsImagen" src="img/eckis.png"></button></td>';
-        echo "</div>";
-        // Botones del padre (ID menu)
-        echo "<div id= 'CajaBtn'>"; //Creamos nuevo permiso para el menu
-        echo '<td><button class="PermsBoton" onclick="getInsertPerms(' . $permisos[0]['id_menu'] . ')"><img class="PermsImagen" src="img/create.png"></button></td>';
-        echo "</div>";
         echo "</div>";
         // Datos del padre
         foreach ($permisos as $permiso) {
@@ -55,46 +55,74 @@ foreach ($perms as $id_menu => $permisos) {
                 }
                 echo "<div class='Caja' id='CajaPadres'>";
                 echo "<p>Permiso: {$permiso['permiso']}</p>";
-                echo "<div id='CajaBtn'>"; // Editamos o eliminamos un permiso
-                echo '<td><button class="PermsBoton" onclick="getUpdatePerms(' . $permiso['id_permiso'] . ')"><img class="PermsImagen" src="img/edit.png"></button></td>';
-                echo '<td><button class="PermsBoton" onclick="getDeletePerms(' . $permiso['id_permiso'] . ')"><img class="PermsImagen" src="img/delete.png"></button></td>';
-                echo "</div>";
+                if (strlen(trim($PRol)) > 0) {
+                        echo '<input type="checkbox" id="PermsCheckbox" onchange="updatePermsProl(' . $permiso['id_permiso'] .", ". $PRol . ')">';
+                } else if (strlen(trim($PUser)) > 0) {
+                        echo '<input type="checkbox" id="PermsCheckbox" onchange="updatePermsPUser(' . $permiso['id_permiso'] .", ". $PUser . ')">';
+                } else {
+                        echo "<div id='CajaBtn'>"; // Editamos o eliminamos un permiso
+                        echo '<td><button class="PermsBoton" onclick="getUpdatePerms(' . $permiso['id_permiso'] . ')"><img class="PermsImagen" src="img/edit.png"></button></td>';
+                        echo '<td><button class="PermsBoton" onclick="getDeletePerms(' . $permiso['id_permiso'] . ')"><img class="PermsImagen" src="img/delete.png"></button></td>';
+                        echo "</div>";
+                }
                 echo "</div>"; // Cerrar CajaPadre
         }
         // Tenemos hijos?
         if (isset($permisos['hijos'])) {
-                echo '<td><button class="PermsBoton" onclick="getInsertSubMenu(' . $permisos[0]['id_menu'] . ')"><img class="PermsImagen" src="img/add.png"></button></td>';
+                if (strlen(trim($PRol)) > 0) {
+                } else if (strlen(trim($PUser)) > 0) {
+                } else {
+                        echo '<td><button class="PermsBoton" onclick="getInsertSubMenu(' . $permisos[0]['id_menu'] . ')"><img class="PermsImagen" src="img/add.png"></button></td>';
+                }
                 foreach ($permisos['hijos'] as $id_menu_hijo => $hijos) {
                         // Los hijos tambien son menus
                         echo "<div class='Caja' id= 'CajaMenu'>";
                         echo "<div id= 'patata'>";
                         echo "<p>{$hijos[0]['titulo']} || con ID: $id_menu_hijo</p>";
 
-                        echo "<div id= 'BotonesMenu'>";
-                        echo '<td><button class="PermsBoton" onclick="getUpdateMenu(' . $id_menu_hijo . ')"><img class="PermsImagen" src="img/option.png"></button></td>';
-                        echo '<td><button class="PermsBoton" onclick="getDeleteMenu(' . $id_menu_hijo . ')"><img class="PermsImagen" src="img/eckis.png"></button></td>';
-                        echo "</div>";
-                        // Botones del hijo
-                        echo "<div id= 'CajaBtn'>"; //Creamos nuevo permiso para el menu del hijo
-                        echo '<td><button class="PermsBoton" onclick="getInsertPerms(' . $id_menu_hijo . ')"><img class="PermsImagen" src="img/create.png"></button></td>';
-                        echo "</div>";
+                        if (strlen(trim($PRol)) > 0) {
+                        } else if (strlen(trim($PUser)) > 0) {
+                        } else {
+                                echo "<div id= 'BotonesMenu'>";
+                                echo '<td><button class="PermsBoton" onclick="getUpdateMenu(' . $id_menu_hijo . ')"><img class="PermsImagen" src="img/option.png"></button></td>';
+                                echo '<td><button class="PermsBoton" onclick="getDeleteMenu(' . $id_menu_hijo . ')"><img class="PermsImagen" src="img/eckis.png"></button></td>';
+                                echo "</div>";
+                                // Botones del hijo
+                                echo "<div id= 'CajaBtn'>"; //Creamos nuevo permiso para el menu del hijo
+                                echo '<td><button class="PermsBoton" onclick="getInsertPerms(' . $id_menu_hijo . ')"><img class="PermsImagen" src="img/create.png"></button></td>';
+                                echo "</div>";
+                        }
                         echo "</div>";
                         // Los datos del hijo
                         foreach ($hijos as $hijo) {
                                 echo "<div class='Caja' id= 'CajaHijos'>";
                                 echo "<p>Permiso: {$hijo['permiso']}</p>";
-                                echo "<div id= 'CajaBtn'>"; // Editamos o eliminamos un permiso
-                                echo '<td><button class="PermsBoton" onclick="getUpdatePerms(' . $hijo['id_permiso'] . ')"><img class="PermsImagen" src="img/edit.png"></button></td>';
-                                echo '<td><button class="PermsBoton" onclick="getDeletePerms(' . $hijo['id_permiso'] . ')"><img class="PermsImagen" src="img/delete.png"></button></td>';
-                                echo "</div>";
+                                if (strlen(trim($PRol)) > 0) {
+                                        echo '<input type="checkbox" id="PermsCheckbox" onchange="updatePermsProl(' . $hijo['id_permiso'] .", ". $PRol . ')">';
+                                } else if (strlen(trim($PUser)) > 0) {
+                                        echo '<input type="checkbox" id="PermsCheckbox" onchange="updatePermsPUser(' . $hijo['id_permiso'] .", ". $PUser . ')">';
+                                } else {
+                                        echo "<div id= 'CajaBtn'>"; // Editamos o eliminamos un permiso
+                                        echo '<td><button class="PermsBoton" onclick="getUpdatePerms(' . $hijo['id_permiso'] . ')"><img class="PermsImagen" src="img/edit.png"></button></td>';
+                                        echo '<td><button class="PermsBoton" onclick="getDeletePerms(' . $hijo['id_permiso'] . ')"><img class="PermsImagen" src="img/delete.png"></button></td>';
+                                        echo "</div>";
+                                }
                                 echo "</div>";
                         }
                         echo "</div>"; // Cerrar CajaMenu para el hijo
-                        echo '<td><button class="PermsBoton" onclick="getInsertMenu(' .  $id_menu_hijo  . ')"><img class="PermsImagen" src="img/add.png"></button></td>';
+                        if (strlen(trim($PRol)) > 0) {
+                        } else if (strlen(trim($PUser)) > 0) {
+                        } else {
+                                echo '<td><button class="PermsBoton" onclick="getInsertMenu(' . $id_menu_hijo . ')"><img class="PermsImagen" src="img/add.png"></button></td>';
+                        }
                 }
         }
         echo "</div>"; // Cerrar CajaMenu para el padre
-        echo '<td><button class="PermsBoton" onclick="getInsertMenu(' . $permisos[0]['id_menu'] . ')"><img class="PermsImagen" src="img/add.png"></button></td>';
+        if (strlen(trim($PRol)) > 0) {
+        } else if (strlen(trim($PUser)) > 0) {
+        } else {
+                echo '<td><button class="PermsBoton" onclick="getInsertMenu(' . $permisos[0]['id_menu'] . ')"><img class="PermsImagen" src="img/add.png"></button></td>';
+        }
 }
 echo "</div>";
 echo "<div class='child' id='OpcionesPerms'></div>";
